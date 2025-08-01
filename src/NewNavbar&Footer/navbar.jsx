44991bbe7b25451024e99cc0e4eb9b3f0405e4cc
_kei_ <116942403+Gcoyy@users.noreply.github.com>
2025-08-01 15:37:07 +0800

    Converted PayslipUI to Tailwind & Added Signature

--- src/NewNavbar&Footer/navbar.jsx
+++ src/NewNavbar&Footer/navbar.jsx
@@ -2,7 +2,6 @@
 
 import { useState } from "react";
 import logo from "../assets/logo.png";
-import "./navbar-footer.css";
 
 export const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -12,17 +11,17 @@ export const Navbar = () => {
   };
 
   return (
-    <header className="navbar">
-      <div className="navbar-container">
-        <div className="navbar-logo">
+    <header className="w-full relative">
+      <div className="w-full h-[55px] md:h-[60px] lg:h-[65px] flex items-center justify-between px-6 md:px-8 lg:px-12 bg-white shadow-md">
+        <div className="flex items-center gap-3">
           <a href="/">
             <img
-              className="logo"
+              className="w-[45px] h-[45px] md:w-[50px] md:h-[50px] lg:w-[55px] lg:h-[55px] object-contain"
               src={logo || "/placeholder.svg"}
               alt="Galanter and Jones Logo"
             />
           </a>
-          <a href="#" className="shop-name">
+          <a href="#" className="font-semibold text-sm text-[#263145]">
             Galanter and Jones
           </a>
         </div>
